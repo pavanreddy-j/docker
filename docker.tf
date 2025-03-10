b54cf3e6301ccabd235d8a15121b24948873df0c
pavan reddy <gvpavanreddy1996@gmail.com>
2025-03-10 07:38:55 +0530

    docker

---
+++ docker.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "expense" {
+    count = 1
+    ami   = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+    instance_type = "t3.micro"
+    # 20GB is not enough
+    root_block_device {
+      volume_size = 50  # Set root volume size to 50GB
+      volume_type = "gp3"  # Use gp3 for better performance (optional)
+    }
+    user_data = file("docker.sh")
+    tags = {
+        Name = "docker"
+    }
+}
+
+resource "aws_security_group" "allow_all_docker" {
+    name = "allow_all_docker"
+    description = "Allow TLS inbound traffice"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags ={
+        Name ="allow_tls"
+    }
+}
